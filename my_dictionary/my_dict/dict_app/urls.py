a537750553f70dda267f54acf9536c692426bc33
Елена <125108381+HelenFri@users.noreply.github.com>
2023-09-29 17:31:31 +0700

    Add pages: add, word_list

--- my_dictionary/my_dict/dict_app/urls.py
+++ my_dictionary/my_dict/dict_app/urls.py
@@ -4,11 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('/home', views.index, name='home'),
-    path('/', views.index, name='home'),
-    path('/words_list', views.words_list, name='list'),
-    path('/words_list/', views.words_list, name='list'),
-    path('/add_word', views.add_word, name='add'),
-    path('/add_word', views.add_word, name='add'),
+    path('home/', views.index, name='home'),
+    path('words_list', views.words_list, name='list'),
+    path('words_list/', views.words_list, name='list'),
+    path('add_word', views.add_word, name='add'),
+    path('add_word/', views.add_word, name='add'),
 
 ]
--- my_dictionary/my_dict/dict_app/views.py
+++ my_dictionary/my_dict/dict_app/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'dict_app/index.html')
+    return render(request, 'dict_app/index.html', context={'page': 'home'})
 
 
 def words_list(request):
-    return render(request, 'dict_app/list.html')
+    return render(request, 'dict_app/list.html', context={'page': 'list'})
 
 
 def add_word(request):
-    return render(request, 'dict_app/add.html')
+    return render(request, 'dict_app/add.html', context={'page': 'add'})
